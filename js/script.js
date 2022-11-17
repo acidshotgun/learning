2ac30c4a9a8ad96c64e55837cf6bb9c3b85c7093
Sasha <example@gmail.com>
2022-11-17 18:44:19 +0300

    start page

--- js/script.js
+++ js/script.js
@@ -2,34 +2,36 @@
 
 //!----------------------------------------------------------------------------------
 
-const objNumber = {
-    numCount: 0,
-    numPlus: 0,
-    numMinus: 0,
-    numList: [],
-    numMax: 0, 
-    numMin: 0
-};
+// const objNumber = {
+//     numCount: 0,
+//     numPlus: 0,
+//     numMinus: 0,
+//     numList: [],
+//     numMax: 0, 
+//     numMin: 0
+// };
 
-objNumber.numCount = +prompt('Сколько чисел?');
+// objNumber.numCount = +prompt('Сколько чисел?');
 
-for (let i = 0; i < objNumber.numCount; i++) {
-    let randomNumber = +prompt('Введи число');
-    if (randomNumber != '' && randomNumber != null) {
-        objNumber.numList[i] = randomNumber;
-        if (randomNumber % 2 == 0) {
-            objNumber.numPlus++;
-        } else {
-            objNumber.numMinus++;
-        }
-    } else {
-        alert('Пустое значение');
-        i--;
-    }
-}
+// for (let i = 0; i < objNumber.numCount; i++) {
+//     let randomNumber = +prompt('Введи число');
+//     if (randomNumber != '' && randomNumber != null) {
+//         objNumber.numList[i] = randomNumber;
+//         if (randomNumber % 2 == 0) {
+//             objNumber.numPlus++;
+//         } else {
+//             objNumber.numMinus++;
+//         }
+//     } else {
+//         alert('Пустое значение');
+//         i--;
+//     }
+// }
 
-objNumber.numMax = Math.max.apply(null, objNumber.numList);
-objNumber.numMin = Math.min.apply(null, objNumber.numList);
+// objNumber.numMax = Math.max.apply(null, objNumber.numList);
+// objNumber.numMin = Math.min.apply(null, objNumber.numList);
 
-console.log(objNumber);
+// console.log(objNumber);
+
+// //!----------------------------------------------------------------------------------
 
